085c55daef8adf1c8356d770761ff9214bd7ab69
Stephen Francis <stephen.francis@rullion.co.uk>
2017-02-27 18:27:08 +0000

    fix render of before/after grid

--- tx/ac_tx_sub_display.js
+++ tx/ac_tx_sub_display.js
@@ -3,53 +3,43 @@
 var UI = require("lazuli-ui/index.js");
 var Data = require("lazuli-data/index.js");
 var SQL = require("lazuli-sql/index.js");
-var IO = require("lazuli-io/index.js");
 
 
 module.exports = UI.Page.clone({
-    id              : "ac_tx_sub_display",
-    entity_id       : "ac_tx_sub",
-    title           : "Record Update",
-    requires_key    : true
+    id: "ac_tx_sub_display",
+    entity_id: "ac_tx_sub",
+    title: "Record Update",
+    requires_key: true,
 });
 
 
 module.exports.sections.addAll([
-    { id: "display", type: "Display" , entity: "ac_tx_sub" },
-    { id: "values" , type: "ListBase", title: "Before/After Values" }
+    { id: "display", type: "Display", entity: "ac_tx_sub" },
+    { id: "values", type: "ListBase", title: "Before/After Values" }
 ]);
 
 
 module.exports.sections.get("values").defbind("setupValues", "setup", function () {
-    var entity;
+    var entity_id = this.owner.page.sections.get("display").fieldset.getField("entity").get();
     this.right_align_numbers = false;
-    this.columns.add({ id: "field" , label: "Field Name"  , visible: true });
+    this.columns.add({ id: "field", label: "Field Name", visible: true });
     this.columns.add({ id: "before", label: "Before Value", visible: true });
-    this.columns.add({ id: "after" , label: "After Value" , visible: true });
-    entity = this.owner.page.sections.get("display").fieldset.getField("entity").get();
-    if (!Data.entities.get(entity)) {
-        this.owner.page.session.messages.add({ text: "Unknown entity: " + entity, type: "E" });
+    this.columns.add({ id: "after", label: "After Value", visible: true });
+    if (!Data.entities.get(entity_id)) {
+        this.owner.page.session.messages.add({ text: "Unknown entity: " + entity_id, type: "E" });
     }
 });
 
 
 module.exports.sections.get("values").override("renderList", function (render_opts) {
-    var entity,
-        key,
-        prev_tx,
-        this_tx,
-        sql,
-        delim = "";
+    var sql;
+    var delim = "";
+    var entity_id = this.owner.page.sections.get("display").fieldset.getField("entity").get();
+    var key = this.owner.page.sections.get("display").fieldset.getField("key_string").get();
+    var prev_tx = this.owner.page.sections.get("display").fieldset.getField("prev_tx").get();
+    var this_tx = this.owner.page.sections.get("display").fieldset.getField("tx").get();
 
-    entity  = this.owner.page.sections.get("display").fieldset.getField("entity"    ).get();
-    key     = this.owner.page.sections.get("display").fieldset.getField("key_string").get();
-    prev_tx = this.owner.page.sections.get("display").fieldset.getField("prev_tx"   ).get();
-    this_tx = this.owner.page.sections.get("display").fieldset.getField("tx"        ).get();
-    if (!Data.entities.get(entity)) {
-        return;
-    }
-    this.record = Data.entities.get(entity).clone({
-        id: entity,
+    this.record = Data.entities.getThrowIfUnrecognized(entity_id).getRecord({
         modifiable: false,
         page: this.owner.page,
         session: this.owner.page.session,
@@ -61,7 +51,7 @@ module.exports.sections.get("values").override("renderList", function (render_op
             delim = ", ";
         }
     });
-    sql += " FROM _history_" + entity + " A WHERE A._key=" + SQL.Connection.escape(key) + " AND A._tx=";
+    sql += " FROM _history_" + entity_id + " A WHERE A._key=" + SQL.Connection.escape(key) + " AND A._tx=";
     this.found1 = false;
     this.found2 = false;
     try {
@@ -79,7 +69,7 @@ module.exports.sections.get("values").override("renderList", function (render_op
         if (!this.found2) {
             this.columns.get("after" ).visible = false;
         }
-        IO.ListBase.renderList.call(this, render_opts);
+        UI.ListBase.renderList.call(this, render_opts);
     } catch (e) {
         this.report(e);
     } finally {
@@ -90,23 +80,21 @@ module.exports.sections.get("values").override("renderList", function (render_op
 
 
 module.exports.sections.get("values").override("renderBody", function (render_opts) {
-    var section = this;
+    var that = this;
     this.record.each(function (field) {
         if (!field.sql_function && !field.getComputed) {
-            section.renderRow(render_opts, field);
-            this.row_count += 1;
+            that.renderRow(render_opts, field);
+            that.row_count += 1;
         }
     });
 });
 
 
 module.exports.sections.get("values").override("renderRow", function (render_opts, field) {
-    var row_elem,
-        css_class,
-        java_str1,
-        java_str2;
-
-    css_class = null;
+    var row_elem;
+    var java_str1;
+    var java_str2;
+    var css_class = null;
     if (this.found1 && this.found2) {
         java_str1 = SQL.Connection.getColumnString(this.resultset1, field.query_column);
         java_str2 = SQL.Connection.getColumnString(this.resultset2, field.query_column);
